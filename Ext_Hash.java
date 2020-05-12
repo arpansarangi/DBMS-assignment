1fb9de2b9027387bb242496ce67ec15b59ba11e3
kushagralavania <50955399+kushagralavania@users.noreply.github.com>
2020-05-12 20:44:03 +0530

    Update Ext_Hash.java

--- Ext_Hash.java
+++ Ext_Hash.java
@@ -1,13 +1,15 @@
 import java.util.*;
 public class Ext_Hash
 {
-	public int glob_depth = 2;
-	public int bfr = 3;
-	public int max_size = 8
-	public int loc_depth[] = new int[max_size];
-	public int bucket[][] = new int[max_size][bfr];
-	public int emp = -100001, maxn = 100001;
-	String get_binary(int n)
+	public int glob_depth = 2;		//global depth
+	public int bfr = 3;		//bfr
+	public int max_size = 8		//maximum number of directories
+	public int loc_depth[] = new int[max_size];		//local depth of all directories
+	public int bucket[][] = new int[max_size][bfr];		//buckets to store numbers
+	public int emp = -100001, maxn = 100001;		//emp to initialize buckets with a value which identifies it as empty, maxn for maximum number of elements
+	
+
+	String get_binary(int n)		//converts hash of number to binary String
 	{
 		int x = n;
 		String ans = "";
@@ -23,7 +25,7 @@ public class Ext_Hash
 	}
 
 
-	String get_directory(String a)
+	String get_directory(String a)		//finds directory using last glob_depth digits of binary String
 	{
 		String ans = "";
 		for(int i = a.length() - 1; i <= a.length() - glob_depth; i--)
@@ -33,7 +35,8 @@ public class Ext_Hash
 		return ans;
 	}
 
-	int get_loc_depth(String k)
+	
+	int get_loc_depth(String k)		//finds local depth of directory
 	{
 		int dec = 0;
 		for(int i = 0; i < k.length(); i++)
@@ -47,7 +50,8 @@ public class Ext_Hash
 		return loc_depth[dec];
 	}
 
-	int get_bucket(int l, String k)
+	
+	int get_bucket(int l, String k)		//finds bucket among local depth number of buckets in the directory
 	{
 		int dec = 0;
 		for(int i = k.length() - 1; i >= k.length() - l; i--)
@@ -60,17 +64,19 @@ public class Ext_Hash
 		return dec;
 	}
 
-	int get_bucket_ind(int b)
+	
+	int get_bucket_ind(int b)		//finds index in bucket where element can be inserted
 	{
 		for(int i = 0; i < bfr; i++)
 		{
 			if(bucket[b][i] == emp)
 				return i;
 		}
-		return -1;
+		return -1;		//bucket is full
 	}
 
-	int find_bucket_ind(int b, int n)
+	
+	int find_bucket_ind(int b, int n)	//finds index of element in bucket being searched
 	{
 		for(int i = 0; i < bfr; i++)
 		{
@@ -79,34 +85,37 @@ public class Ext_Hash
 			if(bucket[b][i] == n)
 				return i;
 		}
-		return -1;
+		return -1;		//element not found
 	}
 
+	
+	
 	void search(int num)
 	{
-		int x = num;
-		String s = get_binary(x % 10);
-		String dir = get_directory(s);
-		int ld = get_loc_depth(dir);
-		int buck = get_bucket(ld, dir);
-		int bucket_ind = find_bucket_ind(buck, num);
+		int x = num;		//copy number
+		String s = get_binary(x % 10);		//binary String of hash
+		String dir = get_directory(s);		//directory of element
+		int ld = get_loc_depth(dir);		//local depth of element
+		int buck = get_bucket(ld, dir);		//bucket number of element
+		int bucket_ind = find_bucket_ind(buck, num);		//the index in bucket where element is present 
 		if(bucket_ind == -1)
 			System.out.println("Element not found");
 		else
-			System.out.println("Element found");
+			System.out.println("Element found");		//Appropriate message
 	}
 
 
+	
 	void insert(int num)
 	{
-		int x = num;
-		String s = get_binary(x % 10);
-		String dir = get_directory(s);
-		int ld = get_loc_depth(dir);
-		int buck = get_bucket(ld, dir);
-		int bucket_ind = get_bucket_ind(buck);
+		int x = num;		//copy number
+		String s = get_binary(x % 10);		//binary String of hash
+		String dir = get_directory(s);		//directory of element
+		int ld = get_loc_depth(dir);		//local depth of element
+		int buck = get_bucket(ld, dir);		//bucket number of element
+		int bucket_ind = get_bucket_ind(buck);		//the index in bucket where element is present 
 		if(bucket_ind != -1)
-			bucket[buck][bucket_ind] = num;
+			bucket[buck][bucket_ind] = num;		//insert element in required position
 		else
 		{
 			int dec = 0;
@@ -118,8 +127,8 @@ public class Ext_Hash
 				if(dig == 1)
 					dec += Math.pow(2, i);
 			}			
-			loc_depth[dec]++;
-			if(ld < glob_depth)
+			loc_depth[dec]++;		//increase local depth
+			if(ld < glob_depth)		//reassign elements to new buckets
 			{
 				int a[] = new int[maxn];
 				int ind = 0;
@@ -131,7 +140,7 @@ public class Ext_Hash
 				for(int i = 0; i < ind; i++)
 					insert(a[ind]);
 			}
-			else
+			else 		//increase global depth as local depth <= global depth and reassign all elements to new buckets
 			{
 				int a[] = new int[maxn];
 				int ind = 0;
@@ -145,7 +154,7 @@ public class Ext_Hash
 						bucket[i][j] = emp;
 					}
 				}
-				glob_depth++;
+				glob_depth++;	
 				for(int i = 0; i < ind; i++)
 					insert(a[ind]);
 			}
@@ -153,21 +162,22 @@ public class Ext_Hash
 	}
 
 
-	public static void main(String args[])
+	
+	public static void main(String args[])		//driver function
 	{
 		Scanner ob = new Scanner(System.in);
 		for(int i = 0; i < max_size; i++)
 		{
-			loc_depth[i] = 1;
+			loc_depth[i] = 1;		//initialize local depth of directories
 		}
 
 		for(int i = 0; i < max_size; i++)
 		{
 			for(int j = 0; j < bfr; j++)
-				bucket[i][j] = emp;
+				bucket[i][j] = emp;		//initialize all buckets to empty
 		}
-		int inp = 0, m;
-		while(inp != 3)
+		int inp = 0, m;		//ind for choice, m for number
+		while(inp != 3)		//exit condition
 		{
 			System.out.println("Enter a choice among the following:");
 			System.out.println("1.Insert");
