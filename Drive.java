5887f92f921cacff5830bee36e2b5be52e3a62bf
kushagralavania <50955399+kushagralavania@users.noreply.github.com>
2020-05-12 23:56:05 +0530

    Add files via upload

---
+++ Drive.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Drive extends Ext_Hash 
+{
+	public static void main(String args[])		//driver function
+	{
+		Ext_Hash obj = new Ext_Hash();
+		Scanner ob = new Scanner(System.in);
+		for(int i = 0; i < obj.max_size; i++)
+		{
+			obj.loc_depth[i] = 1;		//initialize local depth of directories
+		}
+
+		for(int i = 0; i < obj.max_size; i++)
+		{
+			for(int j = 0; j < obj.bfr; j++)
+				obj.bucket[i][j] = obj.emp;		//initialize all buckets to empty
+		}
+		int inp = 0, m;		//ind for choice, m for number
+		while(inp != 3)		//exit condition
+		{
+			System.out.println("Enter a choice among the following:");
+			System.out.println("1.Insert");
+			System.out.println("2.Delete");
+			System.out.println("3.Exit");
+			inp = ob.nextInt();
+			if(inp == 1)
+			{	
+				System.out.println("Enter number to be inserted");
+				m = ob.nextInt();
+				obj.insert(m);
+				System.out.println("Insertion successful");
+			}
+			if(inp == 2)
+			{	
+				System.out.println("Enter number to be searched");
+				m = ob.nextInt();
+				obj.search(m);
+			}
+		}
+	}
+}
